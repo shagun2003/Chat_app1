a8dc853604f2e1a3b0df4f2c6a993bc3d1d5e9b4
Shagun_Singh <134138604+shagun2003@users.noreply.github.com>
2024-07-13 10:59:17 +0530

    Update Cat.js

--- ccat/src/component/Cat/Cat.js
+++ ccat/src/component/Cat/Cat.js
@@ -7,7 +7,7 @@ import send from './send.png';
 import { user } from '../join/join';
 import closeIcon from './closeIcon.png';
 
-const ENDPOINT = 'http://localhost:4500/';
+const ENDPOINT = 'https://chat-app1-m19h.onrender.com';
 let socket;
 
 const Cat = () => {
